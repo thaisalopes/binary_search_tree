1075ce50f99c1fe8132cdd0cc2b5aae442d30f25
ThaisaLopes <ta_lemos@hotmailcom>
2022-06-28 21:05:05 -0300

    Added insert and delete methods

--- binary_search.rb
+++ binary_search.rb
@@ -9,6 +9,8 @@ class Node
 end
 
 class Tree
+  attr_accessor :root
+
   def initialize(array)
     @root = build_tree(array)
   end
@@ -28,6 +30,54 @@ class Tree
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+  def insert(value, root =@root)
+    return Node.new(value) if root.nil?
+    if root.data == value
+      return root
+    elsif root.data < value
+      root.right = insert(value, root.right)
+    else
+      root.left = insert(value, root.left)
+    end
+    return root
+  end
+
+  def delete_value(value, root=@root)
+    return root if root.nil?
+
+    if value < root.data
+      root.left = delete_value(value, root.left)
+    elsif value > root.data
+      root.right = delete_value(value, root.right)
+    else
+      return root.right if root.left.nil?
+      return root.left if root.right.nil?
+      root.data = minValue(root.right)
+      root.right = delete_value(root.data, root.right)
+    end
+    return root
+  end
+
+  def minValue(root)
+    minv = root.data
+    until root.left.nil?
+      minv = root.left.data
+      root = root.left
+    end
+    minv
+  end
+
+  def find(value, root = @root)
+    return "No match" if root.nil?
+    if value == root.data
+      return root
+    elsif value > root.data
+      return find(value, root.right)
+    else
+      return find(value, root.left)
+    end
+  end
+
 end
 
 class Array
@@ -39,4 +89,9 @@ end
 array = [0, 20, -1, 8, 20, 1, 2, 0, 99, 56, 54, 9]
 prepped_array = array.prep_array
 tree = Tree.new(prepped_array)
-tree.pretty_print
+#tree.insert(133)
+#tree.insert(21)
+#tree.pretty_print
+#tree.delete_value(9)
+#tree.pretty_print
+p tree.find(99)
